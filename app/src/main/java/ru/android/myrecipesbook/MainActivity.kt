7cb478cab4776232409e3a512b0d940c399bb0e7
Pavel Semenov <PNSemenov@sberbank.ru>
2022-10-24 20:50:09 +0400

    Добавлены ресурсы и Activity

--- app/src/main/java/ru/android/myrecipesbook/MainActivity.kt
+++ app/src/main/java/ru/android/myrecipesbook/MainActivity.kt
@@ -2,10 +2,12 @@ package ru.android.myrecipesbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Timber.d("This is log for onCreate")
     }
 }
